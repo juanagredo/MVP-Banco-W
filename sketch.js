b6d328a996e1dfed66eb9681e88bca3524ec081e
DESKTOP-NATU1Q4\MI PC <darkfox.19.19.19@gmail.com>
2023-04-10 02:59:16 -0500

    parapa the rapa

--- sketch.js
+++ sketch.js
@@ -1,6 +1,15 @@
 let screens = []; // arreglo para guardar las imágenes
-let currentScreen = 5; //imagen actual
+let currentScreen = 0; //imagen actual
+let boton_position = 0;
+let this_scroll = 0;
+let tut_posX = 131;
+let tut_posx2 = 109;
+let pasos_tut;
 
+let logo = [];
+let section_salvalogo = [];
+let salvalogo_status = 1;
+let section_salvalogo_status = 2;
 
 function preload() {
     //suben las imágenes en una nueva posición
@@ -30,9 +39,19 @@ function preload() {
     screens[13 + 10] = loadImage('./images/dashboard/Ahorro.png');
     screens[14 + 10] = loadImage('./images/dashboard/Crédito.png');
 
+    logo[0] = loadImage('./images/Boton/Boton_activo.png');
+    logo[1] = loadImage('./images/Boton/Boton_inactivo.png');
+
+    section_salvalogo[0] = loadImage('./images/Boton/boton_frame1.png');
+    section_salvalogo[1] = loadImage('./images/Boton/la_seño_explica.png');
+
+    pasos_tut = loadImage('./images/Boton/pasos_tut.png');
 }
 
 function setup() {
+
+    section_salvalogo[2] = null
+
 
 }
 
@@ -44,6 +63,8 @@ function draw() {
     textSize(16);
     textAlign(CENTER);
     text("(" + floor(mouseX) + ", " + floor(mouseY) + ")", mouseX, mouseY);
+    salvalogo_position();
+    show_salvalogo();
 
 }
 
@@ -53,10 +74,65 @@ function mousePressed() {
     nav();
     changeScreen();
     returnPerfil();
+
+    if (salvalogo_status === 0 && section_salvalogo_status === 1) {
+        tut_posx2 -= (256+382)
+        if (tut_posx2 < -2850) {
+            section_salvalogo_status = 0
+            tut_posx2 = 109
+            
+        }
+     }
+
+    button_salvalogo(
+
+        {
+            xButton: 310, //posición x
+            yButton: boton_position, //posición y
+            wButton: 80, //ancho del botón
+            hButton: 80, //alto del boton
+
+        }
+
+    )
+
+    button_tutorial({
+        xButton: 131, //posición x
+        yButton: boton_position - 145, //posición y
+        wButton: 250, //ancho del botón
+        hButton: 30, //alto del boton
+
+    })
+
+    button_tutorial2({
+        xButton: 80, //posición x
+        yButton: boton_position - 300, //posición y
+        wButton: 390, //ancho del botón
+        hButton: 180, //alto del boton
+
+    })
+
+}
+
+function show_salvalogo() {
+    image(logo[salvalogo_status], 306, boton_position);
+
+    if (section_salvalogo_status < 2) {
+        image(section_salvalogo[section_salvalogo_status], tut_posX, boton_position - 230);
+        
+        
+    }
+        if (section_salvalogo_status === 1) {
+        tut_posX = 0
+        image(pasos_tut, tut_posx2, boton_position - 230);
+    } else {
+        tut_posX = 131
+    }
+    
 }
 
 function returnPerfil() {
-    if (currentScreen > 10 + 4 && currentScreen < 10 + 11) {
+    if (currentScreen > 10 + 4 && currentScreen < 10 + 15) {
         button({
             xButton: 20,
             yButton: 20,
@@ -67,6 +143,17 @@ function returnPerfil() {
     }
 }
 
+window.addEventListener('scroll', (e) => {
+    this_scroll = this.scrollY;
+})
+
+function salvalogo_position() {
+    if (window.innerHeight > screens[currentScreen].height) {
+        boton_position = screens[currentScreen].height + this_scroll - 90
+    } else {
+        boton_position = window.innerHeight + this_scroll - 90;
+    }
+}
 
 //para mostrar cada pantalla
 function showScreen() {
@@ -91,6 +178,78 @@ function button({
     const heightB = yButton + hButton;
     if (xButton < mouseX && mouseX < widthB && yButton < mouseY && mouseY < heightB) {
         screenChanger(screen);
+    }
+}
+
+
+function button_tutorial({
+    xButton, //posición x
+    yButton, //posición y
+    wButton, //ancho del botón
+    hButton, //alto del boton
+}) {
+    const widthB = xButton + wButton;
+    const heightB = yButton + hButton;
+    if (xButton < mouseX && mouseX < widthB && yButton < mouseY && mouseY < heightB) {
+
+
+        if (salvalogo_status === 0 && section_salvalogo_status != 1) {
+            section_salvalogo_status = 1
+        }
+    }
+}
+
+function button_tutorial2({
+    xButton, //posición x
+    yButton, //posición y
+    wButton, //ancho del botón
+    hButton, //alto del boton
+}) {
+    const widthB = xButton + wButton;
+    const heightB = yButton + hButton;
+    if (xButton < mouseX && mouseX < widthB && yButton < mouseY && mouseY < heightB) {
+
+
+        if (salvalogo_status === 0 && section_salvalogo_status === 1) {
+           // tut_posx2 += (257+391)
+        }
+    }
+}
+
+function button_salvalogo({
+    xButton, //posición x
+    yButton, //posición y
+    wButton, //ancho del botón
+    hButton, //alto del boton
+}) {
+    const widthB = xButton + wButton;
+    const heightB = yButton + hButton;
+    if (xButton < mouseX && mouseX < widthB && yButton < mouseY && mouseY < heightB) {
+
+
+        if (salvalogo_status === 0) {
+            section_salvalogo_status = 2
+            salvalogo_status = +1
+        } else {
+
+            section_salvalogo_status = 0
+
+            salvalogo_status = 0
+        }
+    }
+}
+
+function buttonToScroll({
+    xButton, //posición x
+    yButton, //posición y
+    wButton, //ancho del botón
+    hButton, //alto del boton
+    toMove
+}) {
+    const widthB = xButton + wButton;
+    const heightB = yButton + hButton;
+    if (xButton < mouseX && mouseX < widthB && yButton < mouseY && mouseY < heightB) {
+        window.scroll(0, toMove);
     }
 }
 
@@ -148,15 +307,15 @@ function nav() {
     }
 
     if (currentScreen === 4) {
-    button({ //Microcredito
-        xButton: 0,
-        yButton: 382,
-        wButton: 390,
-        hButton: 58,
-        screen: 5 // cambien el numero de acuerdo a lo que necesiten 
-    })
+        button({ //Microcredito
+            xButton: 0,
+            yButton: 382,
+            wButton: 390,
+            hButton: 58,
+            screen: 5 // cambien el numero de acuerdo a lo que necesiten 
+        })
 
-}
+    }
 
 }
 
@@ -210,6 +369,14 @@ function changeScreen() {
                 wButton: 295,
                 hButton: 40,
                 screen: 8 // cambien el numero de acuerdo a lo que necesiten 
+            })
+
+            buttonToScroll({
+                xButton: 0,
+                yButton: 657,
+                wButton: 390,
+                hButton: 70,
+                toMove: 1400
             })
 
             break;
@@ -313,6 +480,39 @@ function changeScreen() {
                     });
                 }
             }
+            break;
+
+        case 15:
+
+
+            setTimeout(() => {
+
+                button({
+                    xButton: 16,
+                    yButton: 270,
+                    wButton: 351,
+                    hButton: 121,
+                    screen: 23 // cambien el numero de acuerdo a lo que necesiten 
+                });
+
+                button({
+                    xButton: 16,
+                    yButton: 481,
+                    wButton: 351,
+                    hButton: 111,
+                    screen: 24 // cambien el numero de acuerdo a lo que necesiten 
+                });
+
+                button({
+                    xButton: 16,
+                    yButton: 800,
+                    wButton: 351,
+                    hButton: 121,
+                    screen: 17 // cambien el numero de acuerdo a lo que necesiten 
+                });
+
+            }, 100);
+
             break;
 
 
